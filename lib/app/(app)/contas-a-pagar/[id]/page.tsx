3bfb138c9e3426d1405ee2fdf36430e9f31e8b83
deck03 <contato@deck03.com>
2026-08-19 16:46:58 -0300

    Delete lib/app directory

--- lib/app/(app)/contas-a-pagar/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { EntryDetail } from "@/components/lancamentos/entry-detail";
-
-export default function ContaAPagarDetalhePage({ params }: { params: { id: string } }) {
-  return <EntryDetail entryId={params.id} type="despesa" />;
-}
--- lib/app/(app)/contas-a-receber/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { EntryDetail } from "@/components/lancamentos/entry-detail";
-
-export default function ContaAReceberDetalhePage({ params }: { params: { id: string } }) {
-  return <EntryDetail entryId={params.id} type="receita" />;
-}
